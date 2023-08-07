d492d0b63c96cbef1ca7828eefa51c3972ef90bd
egorlagunovich <egorlagunovich@gmail.com>
2023-08-07 14:13:01 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import Jumbotron from "./components/Jumbotron.jsx";
+import SoundSection from "./components/SoundSection.jsx";
+import DisplaySection from "./components/DisplaySection.jsx";
+import WebgiViewer from "./components/WebgiViewer.jsx";
+import Loader from "./components/Loader.jsx";
+import { useRef } from "react";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  };
 
   return (
     <div className="App">
-      Hello
+      <Loader/>
+      <div ref={contentRef} id="content">
+        <Navbar />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
-
 export default App;
